5d9153731ee94ccfeb6f23ec3693dd1c4f87fd39
Victor Wong <victorhim6@gmail.com>
2022-04-28 17:07:16 +0800

    Update HabitCorrectionActivity.java

--- Andriod/app/src/main/java/com/fyp/sitwell/HabitCorrectionActivity.java
+++ Andriod/app/src/main/java/com/fyp/sitwell/HabitCorrectionActivity.java
@@ -239,8 +239,6 @@ public class HabitCorrectionActivity extends FragmentActivity{
                 );
     }
 
-
-
     private void initiateSocketConnection(String text) {
 
         Log.e("Init", "The initiateSocketConnection " + text);
