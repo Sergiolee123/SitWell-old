598324f5c826cc0e41bee33f867d3ed0d11731cc
Lee Sze Chit <lee.sergio.hk@gmail.com>
2022-05-03 18:18:47 +0800

    add muscle training recommendation

--- Andriod/app/src/main/java/com/fyp/sitwell/muscleTrainingRecommendation/MuscleTrainingRecommendationActivity.java
+++ Andriod/app/src/main/java/com/fyp/sitwell/muscleTrainingRecommendation/MuscleTrainingRecommendationActivity.java
@@ -2,12 +2,18 @@ package com.fyp.sitwell.muscleTrainingRecommendation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.fyp.sitwell.R;
 import com.fyp.sitwell.report.DBHandler;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MuscleTrainingRecommendationActivity extends AppCompatActivity {
 
@@ -17,23 +23,85 @@ public class MuscleTrainingRecommendationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muscle_training_recommendation);
+        TextView textView = findViewById(R.id.recommendation);
+
         dbHandler = new DBHandler(this);
         Cursor sitRecsCursor = dbHandler.getSelectedQuerySitRecs();
         Cursor exRecCursor = dbHandler.getRecentUserExerciseData();
-        //exRecCursor.getInt(2) = strengthExerciseCount, exRecCursor.getInt(3) = relaxExerciseCount
+
+        int leftArm=0, rightArm=0, neck=0, back=0, shoulder=0, sitwell=0, sitbad=0;
+        double strength=0, relax=0;
 
         if(exRecCursor.getCount()!=0){
+            //exRecCursor.getInt(2) = strengthExerciseCount, exRecCursor.getInt(3) = relaxExerciseCount
             exRecCursor.moveToNext();
+            strength=exRecCursor.getInt(2);
+            relax=exRecCursor.getInt(3);
             Log.e("print exRecCurso", exRecCursor.getInt(0)+","+exRecCursor.getString(1)+","+exRecCursor.getInt(2)+","+exRecCursor.getInt(3)+","+exRecCursor.getInt(4));
-            //2:neck 3:back 4:shoulder 5:leftArm 6:rightArm 7:sitWell 8:Sitbad
         }
 
         if(sitRecsCursor.getCount()!=0){
             while( sitRecsCursor.moveToNext())
+                //2:neck 3:back 4:shoulder 5:leftArm 6:rightArm 7:sitWell 8:Sitbad
+                neck+=exRecCursor.getInt(2);
+                back+=exRecCursor.getInt(3);
+                shoulder+=exRecCursor.getInt(4);
+                leftArm+=exRecCursor.getInt(5);
+                rightArm+=exRecCursor.getInt(6);
+                sitwell+=exRecCursor.getInt(7);
+                sitbad+=exRecCursor.getInt(8);
                 Log.e("print sitRecsCursorr", sitRecsCursor.getInt(2)+","+sitRecsCursor.getInt(3)+","+sitRecsCursor.getInt(4)+","+sitRecsCursor.getInt(5)+","+sitRecsCursor.getInt(6)+","+sitRecsCursor.getInt(7)+","+sitRecsCursor.getInt(8));
         }
 
+        SharedPreferences pref = getSharedPreferences("PrefsFile", MODE_PRIVATE);
+        String pref_height = pref.getString("height","");
+        String pref_weight = pref.getString("weight","");
+        String pref_age = pref.getString("age","");
+        double height = Double.parseDouble(pref_height.charAt(0) + "." + pref_height.substring(1));
+        double weight = Double.parseDouble(pref_weight);
 
+        StringBuilder suggest = new StringBuilder();
 
+        if(sitwell>sitbad){
+            suggest.append("You are doing well! Keep going\n");
+        }else{
+            suggest.append("You still need improvement, Keep going\n");
+        }
+
+        if(relax/strength>5){
+            suggest.append("You should do more muscle strength\n");
+        } else if(strength>relax){
+            suggest.append("You should do more muscle relax\n");
+        }
+
+        if(weight/height>24){
+            suggest.append("You should do more core muscle strengthen training\n");
+            suggest.append("You also should control your body fat\n");
+        }
+
+        if(Integer.parseInt(pref_age)>40){
+            suggest.append("You should do more relaxing during your work, you can set the relax alarm period to 1 hour\n");
+        }
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(neck);
+        arrayList.add(back);
+        arrayList.add(shoulder);
+        arrayList.add(leftArm);
+        arrayList.add(rightArm);
+        Collections.sort(arrayList);
+        if(neck==arrayList.get(4)){
+            suggest.append("You should do more neck muscle training\n");
+        } else if(back==arrayList.get(4)){
+            suggest.append("You should do more back muscle training\n");
+        } else if(shoulder==arrayList.get(4)){
+            suggest.append("You should do more shoulder muscle training\n");
+        } else if(leftArm==arrayList.get(4)){
+            suggest.append("You should do more leftArm muscle training\n");
+        } else if(rightArm==arrayList.get(4)){
+            suggest.append("You should do more rightArm muscle training\n");
+        }
+
+        textView.setText(suggest.toString());
     }
 }
